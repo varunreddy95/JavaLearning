d517a6708d373cd8cde40f46f2a7a8870eb5975a
Sai Varun Reddy Kamatham <s6sakama@uni-bonn.de>
2022-10-31 23:54:43 +0100

    Data Types and their attributes

---
+++ DataTypes.java
@@ -0,0 +1,54 @@
+import java.lang.*;
+import java.util.*;
+
+public class DataTypes {
+    public static void main(String[] args){
+
+        //  Data types and their Max, Min and memory values
+
+        System.out.println("******* Integer ********");
+        System.out.println("Max Value: " + Integer.MAX_VALUE);
+        System.out.println("Min Value: " + Integer.MIN_VALUE);
+        System.out.println("Bytes: " + Integer.BYTES);
+        System.out.println("\n");
+
+        System.out.println("******* Short ********");
+        System.out.println("Max Value: " + Short.MAX_VALUE);
+        System.out.println("Min Value: " + Short.MIN_VALUE);
+        System.out.println("Bytes: " + Short.BYTES);
+        System.out.println("\n");
+
+        System.out.println("******* Byte ********");
+        System.out.println("Max Value: " + Byte.MAX_VALUE);
+        System.out.println("Min Value: " + Byte.MIN_VALUE);
+        System.out.println("Bytes: " + Byte.BYTES);
+        System.out.println("\n");
+
+        System.out.println("******* Long ********");
+        System.out.println("Max Value: " + Long.MAX_VALUE);
+        System.out.println("Min Value: " + Long.MIN_VALUE);
+        System.out.println("Bytes: " + Long.BYTES);
+        System.out.println("\n");
+
+        System.out.println("******* Float ********");
+        System.out.println("Max Value: " + Float.MAX_VALUE);
+        System.out.println("Min Value: " + Float.MIN_VALUE);
+        System.out.println("Bytes: " + Float.BYTES);
+        System.out.println("\n");
+
+        System.out.println("******* Double ********");
+        System.out.println("Max Value: " + Double.MAX_VALUE);
+        System.out.println("Min Value: " + Double.MIN_VALUE);
+        System.out.println("Bytes: " + Double.BYTES);
+        System.out.println("\n");
+
+        System.out.println("******* Char ********");
+        System.out.println("Max Value: " + Character.MAX_VALUE);
+        System.out.println("Min Value: " + Character.MIN_VALUE);
+        System.out.println("Bytes: " + Character.BYTES);
+        System.out.println("\n");
+
+        System.out.println("THE END");
+
+    }
+}
