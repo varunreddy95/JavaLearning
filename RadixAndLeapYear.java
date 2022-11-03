2f4e0c0f44c67a2b1ef18d18ffefa90af35f40be
SAI VARUN REDDY <saivarunreddy31@gmail.com>
2022-11-03 14:05:23 +0100

    Conditional and control flow statement practice with challenges

--- RadixAndLeapYear.java
+++ RadixAndLeapYear.java
@@ -63,5 +63,33 @@ public class RadixAndLeapYear {
         {
             System.out.println("No, Not a leap year");
         }
+
+
+        /*
+         ********** Finding the day based on a number **********
+         */
+        System.out.print("Enter the number of the day: ");
+        int day = sc.nextInt();
+
+        if (day==1)
+        {
+            System.out.println("Monday");
+        } else if (day==2) {
+            System.out.println("Tuesday");
+        } else if (day==3) {
+            System.out.println("Wednesday");
+        } else if (day==4) {
+           System.out.println("Thursday");
+        } else if (day==5) {
+            System.out.println("Friday");
+        } else if (day==6) {
+            System.out.println("Saturday");
+        } else if (day==7) {
+            System.out.println("Sunday");
+        }
+        else
+        {
+            System.out.println("Invalid, enter a number between 0 and 7");
+        }
     }
 }
